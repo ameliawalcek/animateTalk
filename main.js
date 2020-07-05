11756c8d73cfe027c2656ba9e3e6a1d3927f9955
ameliawalcek <ameliawalcek@gmail.com>
2020-07-05 09:57:19 +0300

    update

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $('#stop').click(function () {
     $('#box').stop()
 })
 $("#fadeto").click(function () {
-    $('#box').fadeTo("fast", Math.random())
+    $('#box').fadeTo("fast", Math.random()) //600 fast 200 slow
 })
 
 //spider relative
@@ -21,6 +21,8 @@ $('#moveLeft').click(function () {
         left: 0,
         fontSize: '100px',
         opacity: '1'
+    }, function () {
+        $('.fa-spider').css("color", "white")
     })
 })
 
@@ -29,6 +31,8 @@ $('#moveRight').click(function () {
         left: 500,
         fontSize: '350px',
         opacity: '0.5'
+    }, function () {
+        $('.fa-spider').css("color", "#3498db")
     })
 })
 
@@ -49,6 +53,7 @@ $('#moveAround').click(function () {
         .animate({ fontSize: '450px' })
         .animate({ top: 200, fontSize: '1px' })
         .animate({ fontSize: '200px' })
+        .delay(500)
         .animate({ left: 0, top: 100, fontSize: '100px' })
         .animate({ left: 600, top: 300})
         .animate({ left: 0, top: 0 })
@@ -57,3 +62,39 @@ $('#moveAround').click(function () {
 $("#btnhide").click(function () {
     $(".fa-spider").hide(100).show()
 })
+
+// backgroundPositionX
+// backgroundPositionY
+// borderWidth
+// borderBottomWidth
+// borderLeftWidth
+// borderRightWidth
+// borderTopWidth
+// borderSpacing
+// margin
+// marginBottom
+// marginLeft
+// marginRight
+// marginTop
+// opacity
+// outlineWidth
+// padding
+// paddingBottom
+// paddingLeft
+// paddingRight
+// paddingTop
+// height
+// width
+// maxHeight
+// maxWidth
+// minHeight
+// minWidth
+// fontSize
+// bottom
+// left
+// right
+// top
+// letterSpacing
+// wordSpacing
+// lineHeight
+// textIndent
